c600d17cea106ca1cb34e3eafbd280dc2b5409c7
Leonardo da fonseca <leo.fonseca1337@icloud.com>
2022-07-15 08:50:52 -0300

    5

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'leofonsil@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ozvfmchzicnvlyug'
+app.config['MAIL_SERVER']='smtp.educandarioelos.com.br'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'noreply@educandarioelos.com.br'
+app.config['MAIL_PASSWORD'] = 'NoEdu@2022'
 app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['TESTING'] = False
+app.config['DEBUG'] = False
 
 mail = Mail(app)
 
@@ -22,10 +26,10 @@ def mensagem():
   assunto = request.args.get('subject')
   email = request.args.get('email')
   mensagem = request.args.get('message')
+  sender = 'noreply@educandarioelos.com.br'
 
-# recipient = email que vai receber o email
-  msg = Message(sender= email, recipients= ['leo.fonseca1337@icloud.com'])
-  msg.html = "<small>Mensagem enviada pelo formulário do site</small><br/><strong>Enviado por:.</strong> " + nome + ".<br/><strong>Mensagem:. </strong>" + mensagem
+  msg = Message(sender= sender, recipients= ['contato@educandarioelos.com.br'])
+  msg.html = "<small>Mensagem enviada pelo formulário do site</small><br/><br/><strong>Enviado por:.</strong> " + nome + ".<br/><strong>Email:.</strong> "+ email +"<br/><strong>Mensagem:. </strong>" + mensagem
   msg.subject = assunto
   mail.send(msg)
   return render_template('mensagem.html')
